6b9fd518cfc3103f2968389d1cc89a925039221b
jqmtony <jqmtony@192.168.0.19>
2017-09-17 02:31:51 +0800

    初始化导入

---
+++ src/main/java/org/xplus/sample/XplusSbSampleEasyuiApplication.java
@@ -0,0 +1,12 @@
+package org.xplus.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XplusSbSampleEasyuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XplusSbSampleEasyuiApplication.class, args);
+	}
+}
