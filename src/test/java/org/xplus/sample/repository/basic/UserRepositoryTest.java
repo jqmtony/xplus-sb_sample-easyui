e0de7069c28e4e7edf132ff2533cb46b8fed2b1b
Administrator <Administrator@192.168.0.18>
2017-09-27 01:12:42 +0800

    update

---
+++ src/test/java/org/xplus/sample/repository/basic/UserRepositoryTest.java
@@ -0,0 +1,79 @@
+package org.xplus.sample.repository.basic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.xplus.sample.commons.utils.jpa.Criteria;
+import org.xplus.sample.commons.utils.jpa.Criterion;
+import org.xplus.sample.commons.utils.jpa.Projections;
+import org.xplus.sample.commons.utils.jpa.Restrictions;
+import org.xplus.sample.entity.basic.User;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class UserRepositoryTest {
+
+	private static final Logger log = LoggerFactory.getLogger(UserRepositoryTest.class);
+
+	/*
+	 * @Before public void setUp() throws Exception { }
+	 */
+
+	/*
+	 * @After public void tearDown() throws Exception { }
+	 */
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Test
+	public void test() {
+
+		Criteria<User> criteria = new Criteria<>();
+
+		// 等于 name = ‘test’
+		criteria.add(Restrictions.eq("name", "user01"));
+
+		// 等于 name like %test%
+		// criteria.add(Restrictions.like("name", "user01",
+		// Criterion.MatchMode.ANYWHERE));
+
+		// age between(1, 20)
+		// criteria.add(Restrictions.between("age", 1, 20));
+
+		// ISNOTEMPTY(name)
+		// criteria.add(Restrictions.isNotEmpty("name"));
+		// List<String> list = new ArrayList<>();
+		// list.add("Alice");
+		// list.add("Mick");
+		// name in ('Alice','Mick')
+		// criteria.add(Restrictions.in("name", list));
+
+		// length(name) = 5
+		// criteria.add(Restrictions.eq(Projections.Length("name"), 5));
+
+		// max(name) = 5
+		// criteria.add(Restrictions.gt(Projections.Max("name"), 5));
+
+		// (name = 'tt' or name = 'qq')
+		// criteria.add(Restrictions.or(Restrictions.eq("name", "tt"),
+		// Restrictions.eq("name", "qq")));
+
+		List<User> userList = userRepository.findAll(criteria);
+
+		if (!userList.isEmpty()) {
+			for (User user : userList) {
+				log.info(user.toString());
+
+			}
+		}
+	}
+
+}
--- src/test/java/org/xplus/sample/serviceImpl/system/UserServiceImplTest.java
+++ src/test/java/org/xplus/sample/serviceImpl/system/UserServiceImplTest.java
@@ -2,6 +2,7 @@ package org.xplus.sample.serviceImpl.system;
 
 import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,8 +12,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.xplus.sample.commons.utils.jpa.Criteria;
+import org.xplus.sample.commons.utils.jpa.Projections;
+import org.xplus.sample.commons.utils.jpa.Restrictions;
 import org.xplus.sample.entity.basic.User;
 import org.xplus.sample.service.basic.UserService;
 
@@ -27,17 +30,7 @@ public class UserServiceImplTest {
 
 	@Test
 	public void testFindAllIntIntListOfStringListOfString() {
-		// fail("Not yet implemented");
-		int pageNo = 1;
-		int pageSize = 10;
-		List<String> sort = Arrays.asList("id,no".split(","));
-		List<String> order = Arrays.asList("asc,asc".split(","));
-		/*Page<User> page = userService.findAll(pageNo, pageSize, sort, order);
-		if (page.getSize() > 0) {
-			for (User user : page.getContent()) {
-				log.info(user.toString());
-			}
-		}*/
+		
 	}
 
 	@Test
