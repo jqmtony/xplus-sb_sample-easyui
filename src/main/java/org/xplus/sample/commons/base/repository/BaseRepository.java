c09dd25d1085f8c0667b57adb84d4f6b81d83c73
Administrator <Administrator@192.168.0.18>
2017-11-04 15:12:01 +0800

    update

--- src/main/java/org/xplus/sample/commons/base/repository/BaseRepository.java
+++ src/main/java/org/xplus/sample/commons/base/repository/BaseRepository.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
